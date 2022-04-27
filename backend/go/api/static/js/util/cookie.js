6cae790305df76c723db6fc778a1194fd84296f6
shima004 <yellow030408@gmail.com>
2022-04-27 17:17:52 +0900

    add session key

---
+++ backend/go/api/static/js/util/cookie.js
@@ -0,0 +1,10 @@
+const cookie = document.cookie;
+console.log(cookie);
+
+function setToken(token) {
+  document.cookie = "token=" + token + "; path=/; max-age=" + 60 * 60 * 24 * 3 + ";";
+}
+
+function getToken() {
+  return document.cookie.split("token=")[1];
+}
