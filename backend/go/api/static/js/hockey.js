8b8b581e93c688b7f8498ba2cc1be3cd78c5fb4e
shima004 <yellow030408@gmail.com>
2022-05-09 22:38:10 +0900

    tmp comment out

--- backend/go/api/static/js/hockey.js
+++ backend/go/api/static/js/hockey.js
@@ -1,23 +1,23 @@
-const socket = new WebSocket('ws://localhost:8080/ws');
+// const socket = new WebSocket('ws://localhost:8080/ws');
 
-socket.onopen = function (event) {
-  console.log('open');
+// socket.onopen = function (event) {
+//   console.log('open');
 
-  // send json message
-  socket.send(JSON.stringify({ command: 'test', data: 'test2' }));
-}
+//   // send json message
+//   socket.send(JSON.stringify({ command: 'test', data: 'test2' }));
+// }
 
-socket.onmessage = function (event) {
-  console.log('message');
-  console.log(event.data);
-}
+// socket.onmessage = function (event) {
+//   console.log('message');
+//   console.log(event.data);
+// }
 
-socket.onclose = function (event) {
-  console.log('close');
-}
+// socket.onclose = function (event) {
+//   console.log('close');
+// }
 
-socket.onerror = function (event) {
-  console.log('error');
-}
+// socket.onerror = function (event) {
+//   console.log('error');
+// }
 
 
