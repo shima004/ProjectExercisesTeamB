6b881a7bd372dd1eb8b98efc5e42c91c2efcbd85
SakaiHaruto <k21057kk@aitech.ac.jp>
2022-05-10 15:32:27 +0900

    fix load js and css

--- backend/go/api/static/js/blockBreaker.js
+++ backend/go/api/static/js/blockBreaker.js
@@ -1,152 +1,423 @@
 "use strict";
-$("btn1").on("click",function(){
-class Rectangle{
-    constructor(x,y,width,height){
-        this.mWidth = MAG * 4;
-        this.mHeight = MAG * 4;
-    }
 
-    get pCX(){
-        return(this.mX + this.mWidth/2);
-    }
-    set pCX(value){
-        this.mX = value - this.mWidth/2;
-    }
-    get pCY(){
-        return(this.mY + this.mHeight/2);
-    }
-    set pCY(value){
-        this.mY = value - this.mHeight/2;
-    }
-}
+// class Rectangle{
+//     constructor(x,y,width,height){
+//         this.mWidth = width;
+//         this.mHeight = height;
+//     }
+//     contains(x,y){
+//         return(this.mX<x&&x<this.mX+this.mWidth&&this.mY<y&&y<this.mY+this.mHeight);
+//     }
+//     get pCX(){
+//         return(this.mX + this.mWidth/2);
+//     }
+//     set pCX(value){
+//         this.mX = value - this.mWidth/2;
+//     }
+//     get pCY(){
+//         return(this.mY + this.mHeight/2);
+//     }
+//     set pCY(value){
+//         this.mY = value - this.mHeight/2;
+//     }
+// }
 
-const WIDTH = 720;
-const HEIGHT = 540;
-const MESH = 24;
-const MAG = 3;
-const COLUMN = 14;
-const ROW = 8;
+// const WIDTH = 720;
+// const HEIGHT = 540;
+// const MESH = 24;
+// const MAG = 3;
+// const COLUMN = 14;
+// const ROW = 8;
+// const PALETTE = ["#ff0000","#ff00ff","#00ff00","#ffff00"];
 
-var gScore = 0;
-var gLife = 3;
+// var gBreak = [];
+// var gScore = 0;
+// var gLife = 3;
+// var gWait;
+// var gSE = [];
 
-class Ball extends Rectangle{
-    constructor(){
-        super(0, 0, MAG * 4, MAG * 4);
-    }
-    draw(g){
-        g.fillStyle="#ffffff";
-        g.fillRect(this.mX,this.mY,this.mWidth,this.mHeight);
-    }
+// class Ball extends Rectangle{
+//     constructor(){
+//         super(0, 0, MAG * 4, MAG * 4);
+        
+//     }
+//     draw(g){
+//         g.fillStyle="#ffffff";
+//         g.fillRect(this.mX,this.mY,this.mWidth,this.mHeight);
+//     }
     
-    start()
-    {
-        this.pCX = WIDTH/2;
-        this.pCY = MESH * 12;
+//     move(){
+//         this.mX += this.mDX;
+//         this.mY += this.mDY;
+
+//         if(gPlayer.contains(this.mX,this.mY)){
+//             let a = Math.atan2(this.pCY - gPlayer.pCY,this.pCX-gPlayer.pCX);
+//             this.mDX = Math.cos(a);
+//             this.mDY = Math.sin(a);
+//             this.mDY = Math.min(this.mDY,-0.25);
+//             this.mY += this.mDY;
+//         }
+
+//         let x=Math.floor((this.pCX - MESH) / (MESH * 2));
+//         let y=Math.floor((this.pCY - MESH * 3) / MESH);
+
+//         if(x>=0&&x<COLUMN&&
+//             y>=0&&y<ROW){
+//                 let i=y*COLUMN+x;
+//                 if(!gBreak[i]){
+//                     gBreak[i] = 1;
+//                     gScore++;
+//                     let dx = Math.abs(this.pCX - (x + 1)*MESH*2);
+//                     let dy = Math.abs(this.pCY - (y + 3.5) * MESH);
+//                     if(dx<dy*2){
+//                         this.mDY = -this.mDY;
+//                     } else {
+//                         this.mDX = -this.mDX;
+//                     }
+//                 }
+//             }
+//         if(this.pCX<MESH||this.pCX>WIDTH - MESH){
+//             this.mDX = -this.mDX;
+//         }
+//         if(this.pCY<MESH){
+//             this.mDY = -this.mDY;
+//         }
+//     }
+
+//     start(){
+//         this.pCX = WIDTH/2;
+//         this.pCY = MESH * 12;
+//         this.mDX = Math.random()/5-0.1;
+//         this.mDY = 1;
+//         this.mSpeed = 32;
+//     }
+
+//     tick(){
+//         for(let i = 0; i<this.mSpeed/4;i++){
+//             this.move();
+//         }
+//         if(this.mY>HEIGHT){
+//             if(!--gLife){
+//                 return;
+//             }
+//             start();
+//         }
+//     }
+// }
+// var gBall = new Ball();
+
+// class Player extends Rectangle{
+
+//     constructor(){
+//         super(0, 0, MESH*2, MESH);
+//     }
+
+//     draw(g){
+//         DrawBlock(g,this.mX,this.mY,"#00ffff");
+//     }
+
+//     start(){
+//         this.pCX = WIDTH / 2;
+//         this.pCY = HEIGHT - MESH * 2;
+//     }
+
+//     tick(){
+//         this.mX = Math.max(MESH        ,this.mX-gKey[37]*MAG*4);
+//         this.mX = Math.min(WIDTH-MESH*2,this.mX+gKey[39]*MAG*4);
+//         //this.mY = Math.max(MESH        ,this.mY-gKey[38]*MAG*4);
+//         //this.mY = Math.min(HEIGHT-MESH*2,this.mY+gKey[40]*MAG*4);
+//     }
+// }
+
+// var gPlayer = new Player();
+
+// function DrawBlock(g,x,y,style){
+//     g.fillStyle = style;
+//     g.fillRect(x+MAG,y+MAG,MESH*2-MAG*2,MESH-MAG*2);
+// }
+
+// function draw(){
+//     let g = document.getElementById("main").getContext("2d");
+//         g.font = "bold" + MESH * MAG + "px monospace";
+//         g.fillStyle="#ffffff";
+//         g.fillRect(0,0,WIDTH,HEIGHT);
+//         g.fillStyle = "#000000";
+//         g.fillRect(MESH-MAG,MESH-MAG,WIDTH-MESH*2+MAG*2,HEIGHT-MESH+MAG*2);
+
+//         for(let y=0;y<ROW;y++){
+//             for(let x = 0; x<COLUMN;x++){
+//                 if(!gBreak[y*COLUMN+x]){
+//                     DrawBlock(g,MESH*(x*2+1),(y+3)*MESH,PALETTE[y>>1]);
+//                 }
+//             }
+//         }
+
+//         gPlayer.draw(g);
+
+//         g.font="36px monospace";
+//         g.fillStyle="#ffffff";
+//         g.fillText("SCORE "+gScore,MESH*2,MESH*2.5);
+//         g.fillText("LIFE "+gLife,MESH*23,MESH*2.5);
+
+//         if(gLife <= 0){
+//             g.fillText("GAME OVER" ,WIDTH/2-MESH*3,HEIGHT/2+MESH);
+//         }
+//         if(gScore % (COLUMN * ROW)==0){
+//             for(let y=0;y<ROW;y++){
+//                 for(let x=0;x<COLUMN;x++){
+//                     gBreak[y*COLUMN+x]=0;
+//                 }
+//             }
+//         }
+
+//         gBall.draw(g);
+// }
+
+// function start(){
+//     gWait = 60;
+//     gPlayer.start();
+//     gBall.start();
+// }
+
+// function tick(){
+//     if(!gLife){
+//         return;
+//     }
+    
+//     gPlayer.tick();
+//     if(gWait){
+//         gWait--;
+//         return;
+//     }
+//     gBall.tick();
+// }
+
+// // ========================= 01_avoid ==================================
+
+// const TIMER_INTERVAL = 33;
+
+// var gKey = new Uint8Array( 0x100 );
+// var gTimer;
+
+// //描画イベント
+// function onPaint(){
+//     console.log("onPaint" + gTimer);
+//     if(!gTimer){
+//         gTimer = performance.now()
+//     }
+//     if(gTimer + TIMER_INTERVAL<performance.now()){
+//         gTimer+=TIMER_INTERVAL;
+//         tick();
+//         draw();
+//     }
+//     requestAnimationFrame(onPaint);
+// }
+// //キーを押した時のイベント
+// window.onkeydown = function(ev){
+//     gKey[ev.keyCode]=1;
+// }
+// //キーを離した時のイベント
+// window.onkeyup = function(ev){
+//     gKey[ev.keyCode]=0;
+// }
+// //起動時のイベント
+// window.onload=function(){
+//     start();
+
+//     requestAnimationFrame(onPaint);
+// }
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = Math.floor(Math.random()+2);
+var dy = Math.floor(Math.random()+2)*-1;
+var coinRadius = 6;
+var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+var score = 0;
+var lives = 3;
+var Coins = [];
+for(var c=0; c<brickColumnCount; c++) {
+    Coins[c] = [];
+    for(var r=0; r<brickRowCount; r++) {
+        Coins[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
-var gBall = new Ball();
-
-class Player extends Rectangle{
-
-    constructor(){
-        super(0, 0, MESH, MESH);
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(var r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
+}
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
-    draw(g){
-        g.fillStyle="#00ffff";
-        g.fillRect(this.mX,this.mY,MESH,MESH);
-    }
-
-    start(){
-        this.pCX = WIDTH / 2;
-        this.pCY = HEIGHT - MESH * 3;
-    }
-
-    tick(){
-        this.mX = Math.max(MESH        ,this.mX-gKey[37]*MAG*2);
-        this.mX = Math.min(WIDTH-MESH*2,this.mX+gKey[39]*MAG*2);
-        this.mY = Math.max(MESH        ,this.mY-gKey[38]*MAG*2);
-        this.mY = Math.min(HEIGHT-MESH*2,this.mY+gKey[40]*MAG*2);
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
     }
 }
 
-var gPlayer = new Player();
-
-function DrawBlock(g,x,y){
-    g.fillStyle = "#ffff00";
-    g.fillRect(x+MAG,y+MAG,MESH*2-MAG*2,MESH-MAG*2);
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
 }
 
-function draw(){
-    let g = document.getElementById("main").getContext("2d");
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }   
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
 
-        g.fillStyle="#ffffff";
-        g.fillRect(0,0,WIDTH,HEIGHT);
-        g.fillStyle = "#000000";
-        g.fillRect(MESH-MAG,MESH-MAG,WIDTH-MESH*2+MAG*2,HEIGHT-MESH+MAG*2);
 
-        for(let y=0;y<ROW;y++){
-            for(let x = 0; x<COLUMN;x++){
-                DrawBlock(g,MESH*(x*2+1),(y+3)*MESH);
+function collisionDetection() {//衝突判定
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
+            var b = bricks[c][r];
+            if(b.status == 1) {
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status=0;
+                    score++;
+                    if(score%15==0) {//ブロック復活
+                        for(var c=0;c<brickColumnCount;c++){
+                            for(var r=0;r<brickRowCount;r++){
+                                bricks[c][r].status=1;
+                            }
+                        }
+                        if(paddleWidth>20){//パドル大きさ変更
+                            paddleWidth=paddleWidth-5;
+                        }
+                        if(ballRadius>20){//ボール大きさ変更
+                            ballRadius=ballRadius-5;
+                        }
+                    }
+                }
+            } 
+        }
+    }
+}
+
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#ff0000";
+    ctx.fillText("Score: "+score, 8, 20);
+}
+
+function drawLives() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#00ff00";
+    ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+}
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawBricks() {
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
+            if(bricks[c][r].status == 1) {
+                var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#ff0000";
+                ctx.fill();
+                ctx.closePath();
             }
         }
-
-        gPlayer.draw(g);
-
-        g.font="36px monospace";
-        g.fillStyle="#ffffff";
-        g.fillText("SCORE "+gScore,MESH*2,MESH*2.5);
-        g.fillText("LIFE "+gLife,MESH*23,MESH*2.5);
-
-        if(gLife <= 0){
-            g.fillText("GAME OVER" ,WIDTH/2-MESH*3,HEIGHT/2);
+    }
+}
+function drawCoins() {
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
+              var CoinX = (c*(brickWidth+brickPadding))+brickOffsetLeft+brickWidth/2;
+              var CoinY = (r*(brickHeight+brickPadding))+brickOffsetTop+brickHeight/2;
+              Coins[c][r].x = CoinX;
+              Coins[c][r].y = CoinY;
+              ctx.beginPath();
+              ctx.arc(CoinX, CoinY, coinRadius,0, Math.PI*2);
+              ctx.fillStyle = "#ffff00";
+              ctx.fill();
+              ctx.closePath();
         }
-
-        gBall.draw(g);
-}
-
-function start(){
-    gPlayer.start();
-    gBall.start();
-}
-
-function tick(){
-    gPlayer.tick();
-}
-
-// ========================= 01_avoid ==================================
-
-const TIMER_INTERVAL = 33;
-
-var gKey = new Uint8Array( 0x100 );
-var gTimer;
-
-//描画イベント
-function onPaint(){
-    console.log("onPaint" + gTimer);
-    if(!gTimer){
-        gTimer = performance.now()
     }
-    if(gTimer + TIMER_INTERVAL<performance.now()){
-        gTimer+=TIMER_INTERVAL;
-        tick();
-        draw();
-    }
-    requestAnimationFrame(onPaint);
 }
-//キーを押した時のイベント
-window.onkeydown = function(ev){
-    gKey[ev.keyCode]=1;
-}
-//キーを離した時のイベント
-window.onkeyup = function(ev){
-    gKey[ev.keyCode]=0;
-}
-//起動時のイベント
-window.onload=function(){
-    start();
 
-    requestAnimationFrame(onPaint);
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
+    drawBall();
+    drawPaddle();
+    drawCoins();
+    drawScore();
+    drawLives();
+    collisionDetection();
+    x += dx;
+    y += dy;
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    } else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            lives--;
+            if(!lives) {
+                alert("GAME OVER");
+                document.location.reload();
+            }
+            else {
+                x = canvas.width/2;
+                y = canvas.height-30;
+                dx = Math.floor(Math.random()+2);
+                dy = Math.floor(Math.random()+2)*-1;
+                paddleX = (canvas.width-paddleWidth)/2;
+            }
+        }
+    }
 }
-});
+
+
+var interval = setInterval(draw, 10);
