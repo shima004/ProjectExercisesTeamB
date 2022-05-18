3b96a32eb0801ec09330ff8b953a00e64e4bf23e
SakaiHaruto <k21057kk@aitech.ac.jp>
2022-05-18 13:47:15 +0900

    change ballspeed,font

--- backend/go/api/static/js/blockBreaker.js
+++ backend/go/api/static/js/blockBreaker.js
@@ -230,9 +230,9 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height-30;
-var dx = Math.floor(Math.random()+2);
-var dy = Math.floor(Math.random()+2)*-1;
-var ballRadius = 8;
+var dx = Math.random()*2+2;
+var dy = (Math.random()*2+2)*-1;
+var ballRadius = 10;
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
@@ -243,7 +243,7 @@ var brickColumnCount = 7;
 var brickWidth = 60;
 var brickHeight = 20;
 var brickPadding = 10;
-var brickOffsetTop = 30;
+var brickOffsetTop = 60;
 var brickOffsetLeft = 50;
 var score = 0;
 var lives = 3;
@@ -307,9 +307,6 @@ function collisionDetection() {//衝突判定
                             paddleWidth=paddleWidth-5;
                         }
                         level++;
-                        if(paddleWidth>20){//パドル大きさ変更
-                            paddleWidth=paddleWidth-5;
-                        }
                         if(dx>=0){
                             dx++;
                         } else {
@@ -328,15 +325,15 @@ function collisionDetection() {//衝突判定
 }
 
 function drawScore() {
-    ctx.font = "16px Arial";
+    ctx.font = "32px Arial";
     ctx.fillStyle = "#ff0000";
-    ctx.fillText("Score: "+score, 8, 20);
+    ctx.fillText("Score: "+score, 8, 40);
 }
 
 function drawLives() {
-    ctx.font = "16px Arial";
+    ctx.font = "32px Arial";
     ctx.fillStyle = "#00ff00";
-    ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+    ctx.fillText("Lives: "+lives, canvas.width-150, 40);
 }
 
 function drawBall() {
