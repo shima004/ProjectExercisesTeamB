4df8255f2c31a151e55495c516f0ae52fa28059c
miura13 <k21113kk@aitech.ac.jp>
2022-05-18 13:09:16 +0900

    slotJS

--- backend/go/api/static/js/slot.js
+++ backend/go/api/static/js/slot.js
@@ -31,12 +31,15 @@
             let element = document.getElementById('hanteii');
             element.value = 'SPINNING';
 
+            let element2 = document.getElementById('temotikoinn');
+            element2.value = '100';
+
 
             var loopup1 = function () {
 
                 document.getElementById('img1').src = img[cg1++];
 
-                if (cg1 > 5) {
+                if (cg1 > 4) {
                     cg1 = 0;
                 }
 
@@ -48,7 +51,7 @@
 
                 document.getElementById('img2').src = img[cg2++];
 
-                if (cg2 > 5) {
+                if (cg2 > 4) {
                     cg2 = 0;
                 }
 
@@ -59,7 +62,7 @@
             var loopup3 = function () {
                 document.getElementById('img3').src = img[cg3++];
 
-                if (cg3 > 5) {
+                if (cg3 > 4) {
                     cg3 = 0;
                 }
             }
