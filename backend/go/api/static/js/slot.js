27dcef80f6961bced2e1ca723652c3bf9b3c9935
miura13 <k21113kk@aitech.ac.jp>
2022-06-07 14:55:24 +0900

    s

--- backend/go/api/static/js/slot.js
+++ backend/go/api/static/js/slot.js
@@ -3,10 +3,7 @@
 
         var sw;
         stt = 0;
-        var chec;
-
-        var ataricnt;
-        ataricnt = 0;
+        var chec=0;
 
         var flag;
         var coincnt;
@@ -76,57 +73,115 @@
                     }
                 }
                 sp3 = setInterval(loopup3, 150);
-            //}
+        }
+
+
+        function setImage() {
+
+            cg1 = 0;
+            cg2 = 0;
+            cg3 = 0;
+
+            stop1 = 0;
+            stop2 = 0;
+            stop3 = 0;
+
+            var img = ["../static/img/pt1.png", "../static/img/pt2.png", "../static/img/pt3.png", "../static/img/pt4.png", "../static/img/pt5.png"];
+
+            let element = document.getElementById('hanteii');
+            element.value = 'SPINNING';
+
+
+            var loopup1 = function () {
+
+                document.getElementById('img1').src = img[cg1++];
+
+                if (cg1 > 4) {
+                    cg1 = 0;
+                }
+
+            }
+
+            sp1 = setInterval(loopup1, 130);
+
+            var loopup2 = function () {
+
+                document.getElementById('img2').src = img[cg2++];
+
+                if (cg2 > 4) {
+                    cg2 = 0;
+                }
+
+            }
+
+
+            sp2 = setInterval(loopup2, 90);
+
+            var loopup3 = function () {
+                document.getElementById('img3').src = img[cg3++];
+
+                if (cg3 > 4) {
+                    cg3 = 0;
+                }
+            }
+            sp3 = setInterval(loopup3, 70);
         }
 
 
         function slot1_stop() {
             clearInterval(sp1);
             stop1 = 1;
-
-            if (elements[0].checked == true) {
+            /*if (document.COIN.bet[0].checked == true) {
                 chec = 20;
-            } else if (elements[1].checked == true) {
+            } else if (document.COIN.bet[1].checked == true) {
                 chec = 50;
-            } else if (elements[2].checked == true) {
+            } else if (document.COIN.bet[2].checked == true) {
                 chec = 100;
-            } else if (elements[3].checked == true) {
+            } else if (document.COIN.bet[3].checked == true) {
                 chec = 150;
-            }
+            }*/
+
 
 
             if (stop1 == 1 && stop2 == 1 && stop3 == 1) {
                 if (cg1 == cg2 && cg1 == cg3) {
                     let element = document.getElementById('hanteii');
                     element.value = '~WIN!!~';
-                    ataricnt = ataricnt + 1;
-                    if (ataricnt >= 2) {
-                        check = check * 1.5;
+                    let element2 = document.getElementById('betkoinn');
+                    element2.value = "0枚";
+                    if (cg1==0) {
+                        coincnt = chec * 1.2;
+                        console.log(coincnt);
+                        postCoin(coincnt);
+                        alert(coincnt + "枚ゲットしました！")   //alert
+                    } else if (cg1==1) {
+                        coincnt = chec * 1.5;
+                        console.log(coincnt);
+                        postCoin(coincnt);
+                        alert(coincnt + "枚ゲットしました！")   //alert
+                    } else if (cg1==2) {
+                        coincnt = chec * 1.7;
+                        console.log(coincnt);
+                        postCoin(coincnt);
+                        alert(coincnt + "枚ゲットしました！")   //alert
+                    } else if (cg1==3) {
+                        coincnt = chec * 2.0;
+                        console.log(coincnt);
+                        postCoin(coincnt);
+                        alert(coincnt + "枚ゲットしました！")   //alert
+                    } else if (cg1==4) {
+                        coincnt = chec * 2.5;
+                        console.log(coincnt);
+                        postCoin(coincnt);
+                        alert(coincnt + "枚ゲットしました！")   //alert
                     }
-                        if (cg1==1) {
-                            coincnt = chec * 1.2;
-                            console.log(coincnt);
-                            postCoin(coincnt);
-                        } else if (cg==2) {
-                            coincnt = chec * 1.5;
-                            console.log(coincnt);
-                            postCoin(coincnt);
-                        } else if (cg==3) {
-                            coincnt = chec * 1.7;
-                            console.log(coincnt);
-                            postCoin(coincnt);
-                        } else if (cg==4) {
-                            coincnt = chec * 2.0;
-                            console.log(coincnt);
-                            postCoin(coincnt);
-                        }
-
                 } else {
                     let element = document.getElementById('hanteii');
                     element.value = '~lose(´ω`)~';
-                    ataricnt = 0;
-                    postCoin(chec);
-                    console.log(chec);
+                    let element2 = document.getElementById('betkoinn');
+                    element2.value = "0枚";
+                    postCoin(-chec);
+                    console.log(-chec);
                 }
             }
             sw = 0;
@@ -137,51 +192,59 @@
             clearInterval(sp2);
             stop2 = 1;
 
-            if (elements[0].checked == true) {
+
+            /*if (document.COIN.bet[0].checked == true) {
                 chec = 20;
-            } else if (elements[1].checked == true) {
+            } else if (document.COIN.bet[1].checked == true) {
                 chec = 50;
-            } else if (elements[2].checked == true) {
+            } else if (document.COIN.bet[2].checked == true) {
                 chec = 100;
-            } else if (elements[3].checked == true) {
+            } else if (document.COIN.bet[3].checked == true) {
                 chec = 150;
-            }
+            }*/
 
             if (stop1 == 1 && stop2 == 1 && stop3 == 1) {
                 if (cg1 == cg2 && cg1 == cg3) {
                     let element = document.getElementById('hanteii');
                     element.value = '~WIN!!~';
-                    ataricnt = ataricnt + 1;
-                    if (ataricnt >= 2) {
-                        check = check * 1.5;
-                    }
-                    if (cg1==1) {
+                    let element2 = document.getElementById('betkoinn');
+                    element2.value = "0枚";
+                    if (cg1==0) {
                         coincnt = chec * 1.2;
                         console.log(coincnt);
                         postCoin(coincnt);
-                    } else if (cg==2) {
+                        alert(coincnt + "枚ゲットしました！")   //alert
+                    } else if (cg1==1) {
                         coincnt = chec * 1.5;
                         console.log(coincnt);
                         postCoin(coincnt);
-                    } else if (cg==3) {
+                        alert(coincnt + "枚ゲットしました！")   //alert
+                    } else if (cg1==2) {
                         coincnt = chec * 1.7;
                         console.log(coincnt);
                         postCoin(coincnt);
-                    } else if (cg==4) {
+                        alert(coincnt + "枚ゲットしました！")   //alert
+                    } else if (cg1==3) {
                         coincnt = chec * 2.0;
                         console.log(coincnt);
                         postCoin(coincnt);
+                        alert(coincnt + "枚ゲットしました！")   //alert
+                    } else if (cg1==4) {
+                        coincnt = chec * 2.5;
+                        console.log(coincnt);
+                        postCoin(coincnt);
+                        alert(coincnt + "枚ゲットしました！")   //alert
                     }
 
                 } else {
                     let element = document.getElementById('hanteii');
                     element.value = '~lose(´ω`)~';
-                    ataricnt = 0;
-                    postCoin(chec);
-                    console.log(chec);
+                    let element2 = document.getElementById('betkoinn');
+                    element2.value = "0枚";
+                    postCoin(-chec);
+                    console.log(-chec);
                     
                 }
-            //sw = 0;
             }
         }
 
@@ -190,50 +253,57 @@
             clearInterval(sp3);
             stop3 = 1;
 
-            if (elements[0].checked == true) {
+            /*if (document.COIN.bet[0].checked == true) {
                 chec = 20;
-            } else if (elements[1].checked == true) {
+            } else if (document.COIN.bet[1].checked == true) {
                 chec = 50;
-            } else if (elements[2].checked == true) {
+            } else if (document.COIN.bet[2].checked == true) {
                 chec = 100;
-            } else if (elements[3].checked == true) {
+            } else if (document.COIN.bet[3].checked == true) {
                 chec = 150;
-            }
+            }*/
 
             if (stop1 == 1 && stop2 == 1 && stop3 == 1) {
                 if (cg1 == cg2 && cg1 == cg3) {
                     let element = document.getElementById('hanteii');
                     element.value = '~WIN!!~';
-                    ataricnt = ataricnt + 1;
-                    if (ataricnt >= 2) {
-                        check = check * 1.5;
-                    }
-                    if (cg1==1) {
+                    let element2 = document.getElementById('betkoinn');
+                    element2.value = "0枚";
+                    if (cg1==0) {
                         coincnt = chec * 1.2;
                         console.log(coincnt);
                         postCoin(coincnt);
-                    } else if (cg==2) {
+                        alert(coincnt + "枚ゲットしました！")   //alert
+                    } else if (cg1==1) {
                         coincnt = chec * 1.5;
                         console.log(coincnt);
                         postCoin(coincnt);
-                    } else if (cg==3) {
+                        alert(coincnt + "枚ゲットしました！")   //alert
+                    } else if (cg1==2) {
                         coincnt = chec * 1.7;
                         console.log(coincnt);
                         postCoin(coincnt);
-                    } else if (cg==4) {
+                        alert(coincnt + "枚ゲットしました！")   //alert
+                    } else if (cg1==3) {
                         coincnt = chec * 2.0;
                         console.log(coincnt);
                         postCoin(coincnt);
+                        alert(coincnt + "枚ゲットしました！")   //alert
+                    } else if (cg1==4) {
+                        coincnt = chec * 2.5;
+                        console.log(coincnt);
+                        postCoin(coincnt);
+                        alert(coincnt + "枚ゲットしました！")   //alert
                     }
-                    
+
                 } else {
                     let element = document.getElementById('hanteii');
                     element.value = '~lose(´ω`)~';
-                    ataricnt = 0;
-                    postCoin(chec);
-                    console.log(chec);
+                    let element2 = document.getElementById('betkoinn');
+                    element2.value = "0枚";
+                    postCoin(-chec);
+                    console.log(-chec);
                 }
-            //sw = 0;
             }
         }
 
@@ -250,11 +320,21 @@
             }
         }
 
-
         function slot_bet() {
             sw = 1;
-            //document.getElementById('srt').disabled = false;
-            //let formElements = document.forms.contactForm;
-            //formElements.submit.disabled = false;
-        }
+            if (document.COIN.bet[0].checked == true) {
+                chec = 20;
+            } else if (document.COIN.bet[1].checked == true) {
+                chec = 50;
+            } else if (document.COIN.bet[2].checked == true) {
+                chec = 100;
+            } else if (document.COIN.bet[3].checked == true) {
+                chec = 150;
+            }
 
+            let element2 = document.getElementById('betkoinn');
+            element2.value = chec + "枚";
+            alert(chec + "枚ベットしました");
+            //postCoin(2000);
+            //console.log(-chec);
+        }
