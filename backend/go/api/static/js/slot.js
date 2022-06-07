b05e6bfc32506a6b52d5ca5eb882a1e3f5d91430
miura13 <k21113kk@aitech.ac.jp>
2022-06-07 16:39:38 +0900

    slotjs

--- backend/go/api/static/js/slot.js
+++ backend/go/api/static/js/slot.js
@@ -343,13 +343,13 @@
             }
 
             if (document.COIN.bet[0].checked == true) {
-                document.COIN.bet[0].checked == false
+                document.COIN.bet[0].checked = false
             } else if (document.COIN.bet[1].checked == true) {
-                document.COIN.bet[1].checked == false
+                document.COIN.bet[1].checked = false
             } else if (document.COIN.bet[2].checked == true) {
-                document.COIN.bet[2].checked == false
+                document.COIN.bet[2].checked = false
             } else if (document.COIN.bet[3].checked == true) {
-                document.COIN.bet[3].checked == false
+                document.COIN.bet[3].checked = false
             }
 
         }
